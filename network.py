e6e914b9fec62661964204428f65f0210d41e6fe
Philip Xu <philipxjm1@gmail.com>
2015-10-20 02:18:28 -0400

    started to feed information into the network

--- network.py
+++ network.py
@@ -4,6 +4,8 @@ import math
 import random
 import string
 import numpy as np
+import csv
+import binascii
 
 random.seed(0)
 
@@ -60,7 +62,7 @@ class Network:
         # input activations
         for i in range(self.ni-1):
             #self.ai[i] = sigmoid(inputs[i])
-            self.ai[i] = inputs[i]
+            self.ai[i] = float(inputs[i]);
 
         # hidden activations
         for j in range(self.nh-1):
@@ -86,7 +88,7 @@ class Network:
         # calculate error terms for output
         output_deltas = [0.0] * self.no
         for k in range(self.no):
-            error = targets[k]-self.ao[k]
+            error = float(targets[k])-self.ao[k]
             output_deltas[k] = dsigmoid(self.ao[k]) * error
 
         # calculate error terms for hidden
@@ -115,13 +117,21 @@ class Network:
         # calculate error
         error = 0.0
         for k in range(len(targets)):
-            error = error + 0.5*(targets[k]-self.ao[k])**2
+            error = error + 0.5*(float(targets[k])-self.ao[k])**2
         return error
 
 
-    def test(self, patterns):
-        for p in patterns:
-            print(p[0], '->', self.update(p[0]))
+    def test(self, data):
+        for p in data:
+            binList = self.update(p[1:]);
+            binStr = "";
+            for x in binList:
+                if(x <= 0):
+                    binStr = binStr + str(0);
+                else:
+                    binStr = binStr + str(1);
+            print(binStr);
+            #print(str(p[0]) + '->' + self.fromBinaryToCharacter(binStr));
 
     def weights(self):
         print('Input weights:')
@@ -131,37 +141,49 @@ class Network:
         for j in range(self.nh):
             print(self.wo[j])
 
-    def train(self, patterns, iterations=1000, N=0.5, M=0.1):
+    def train(self, data, iterations=100, N=0.05, M=0.1):
         # N: learning rate
         # M: momentum factor
         for i in range(iterations):
             error = 0.0
-            for p in patterns:
-                inputs = p[0]
-                targets = p[1]
-                self.update(inputs)
-                error = error + self.backPropagate(targets, N, M)
-            if i % 100 == 0:
-                print('error %-.5f' % error)
+            #print(data[1:]);
+            inputs = data[1:];
+            targets = self.fromCharacterToBinary(data[0]);
+            self.update(inputs)
+            error = error + self.backPropagate(targets, N, M)
+            #if i % 100 == 0:
+            #    print('error %-.5f' % error)
 
+    def fromCharacterToBinary(self, char):
+        binStr = str(bin(int(binascii.hexlify(char), 16)))[2:];
+        while(len(binStr) != 7):
+            binStr = str(0) + binStr;
+        #print(binStr);
+        return binStr;
+
+    def fromBinaryToCharacter(self, binStr):
+        n = int("0b" + binStr, 2);
+        #print(binStr);
+        character = binascii.unhexlify(str(n));
+        return character;
 
 def demo():
-    # Teach network XOR function
-    pat = [
-        [[0,0], [0]],
-        [[0,1], [1]],
-        [[1,0], [1]],
-        [[1,1], [0]]
-    ]
+    trainingData = importCSV("param1.csv");
+    #print(trainingData);
 
     # create a network with two input, two hidden, and one output nodes
-    n = Network(13, 2, 36)
-    # train it with some patterns
-    n.train(pat)
+    n = Network(13, 26, 7)
+    # train it with some data
+    for x in range(len(trainingData)):
+        n.train(trainingData[x], iterations=1, N=0.05);
     # test it
-    n.test(pat)
+    n.test(trainingData);
 
-
+def importCSV(csvURL):
+    with open(csvURL, 'rb') as f:
+        reader = csv.reader(f);
+        trainingData = list(reader);
+        return trainingData;
 
 if __name__ == '__main__':
     demo()
